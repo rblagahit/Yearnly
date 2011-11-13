68f067700ec392ce5e03319e3c4ce86c9801d775
Brent Coney <coney.brent@gmail.com>
2011-11-13 09:23:43 -0600

    Added smarter errors
    The errors now use the user class to output errors.

--- authenticate.php
+++ authenticate.php
@@ -3,8 +3,7 @@ require_once("classes/AutoLoader.php");
 session_unset();
 $user = new User($_POST);
 if(!$user->IsAuthenticated()){
-	$error = base64_encode("Wrong email or password.");
-	header("Location:index.php?id=".$error);
+	header("Location:index.php?id=".$user->GetErrorString());
 }else{
 	header("Location: home.php");
 }
